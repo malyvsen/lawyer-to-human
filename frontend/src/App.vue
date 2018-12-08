096cc0ea748351b47ffa6af41a7bc2e3e8d985af
Franciszek Madej <f.madej@protonmail.com>
2018-12-08 18:49:25 +0100

    Add initial Vue setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <PaperDocument></PaperDocument>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import PaperDocument from './components/PaperDocument'
+
+export default {
+  name: 'App',
+  components: {
+    PaperDocument
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
