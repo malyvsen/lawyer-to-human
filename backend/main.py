56ca150b66cd5566a7d6d70f8a405742ca280d91
Beastie <u@1ux.org>
2018-12-09 02:32:56 +0100

    Implement TTS  - func generation  - audio file serving

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,39 @@
 import binascii
 import random
 import os
-from flask import Flask, flash, request, redirect, url_for
+import string
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
+from gtts import gTTS
 
 
 UPLOAD_FOLDER = '/tmp'
+TTS_FOLDER = 'tts'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TTS_FOLDER'] = os.path.join(UPLOAD_FOLDER, TTS_FOLDER)
 
 CORS(app)
 
 NO_FILE_UPLOADED = 'NO FILE UPLOADED'
+
+def gen_tts(txt):
+    random_hash = binascii.hexlify(os.urandom(16)).decode('utf-8')
+    filename = f"{random_hash}.mp3"
+    tts = gTTS(text=txt, lang='pl')
+    directory = app.config['TTS_FOLDER']
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    tts.save(os.path.join(directory, filename))
+    return filename
+
+
+@app.route('/audio/<path:path>')
+def serve_audio(path):
+    return send_from_directory(app.config['TTS_FOLDER'], path)
+
 
 @app.route('/', methods=['POST'])
 def index():
