cf04922910d84b8251bcae2957930d936111bbbe
Gulio <thegulio@gmail.com>
2018-12-09 10:21:54 +0100

    added 1 red flag and 1 red flag document

---
+++ backend/text_processing/red_flags/__init__.py
@@ -0,0 +1,3 @@
+definitions = {
+'strony wyłączają uprawnienia [\w]+ do dochodzenia roszczeń z tytułu wad [\w]+ ujawnionych po dniu zakupu.': 'Nielegalny zapis. Sprzedawca nie może odmówić reklamacji z powodu ukrytych wad.'
+}
