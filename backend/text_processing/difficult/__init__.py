f1517776ebe7892a02ee0668c0905fa21cc6b19e
malyvsen <iceflamecode@gmail.com>
2018-12-09 02:58:32 +0100

    Analysis includes difficult words

---
+++ backend/text_processing/difficult/__init__.py
@@ -0,0 +1,11 @@
+definitions = {
+'statut': 'regulamin, kodeks',
+'dyskryminacja': 'nękanie',
+'pochodzenie etniczne': 'rasa',
+'werbalny': 'słowny, ustny',
+'pozawerbalny': 'przekazywany czymś innym niż słowami',
+'mobbing': 'nękanie, przemoc psychiczna',
+'wyelminowanie': 'pozbycie się',
+'izolować': 'unikać, trzymać w odosobnieniu',
+'zadośćuczynienie': 'odszkodowanie'
+}
--- backend/text_processing/difficult_dictionary.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def read_dict(self, path):
-    with open(path, 'r') as f:
-        return json.load(f)
