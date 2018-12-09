f3d2b0db096a62e36205e1902cb5a75a6f43614c
Franciszek Madej <f.madej@protonmail.com>
2018-12-09 13:13:47 +0100

    Add tooltips for navigation buttons

--- frontend/src/components/SentenceNav.vue
+++ frontend/src/components/SentenceNav.vue
@@ -2,38 +2,61 @@
   <nav class="sentence-nav">
     <v-layout justify-space-between row>
       <v-flex sm1 class="text-xs-center">
-        <v-btn flat icon @click="$emit('previousSentence')">
-          <v-icon>arrow_back</v-icon>
-        </v-btn>
+        <v-tooltip bottom>
+          <v-btn slot="activator" flat icon @click="$emit('previousSentence')">
+            <v-icon>arrow_back</v-icon>
+          </v-btn>
+          <span>Przejdź do poprzedniego zdania.</span>
+        </v-tooltip>
       </v-flex>
 
       <v-flex class="text-xs-center">
-        <v-item-group>
-          <span style="margin-right: 1rem">
-            <strong>{{ sentenceIndex }}</strong> / {{ '30' - 1 }}
-          </span>
+          <v-item-group>
+            <v-tooltip bottom>
+              <span slot="activator" style="margin-right: 1rem">
+                <strong>{{ sentenceIndex }}</strong> / {{ '30' - 1 }}
+              </span>
+              <span>
+               Twoja pozycja w dokumencie - obecne zdanie / liczba zdań.
+              </span>
+            </v-tooltip>
+
           <v-divider vertical></v-divider>
 
-          <v-btn flat icon>
-            <v-icon>volume_up</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <v-btn slot="activator" flat icon>
+              <v-icon>volume_up</v-icon>
+            </v-btn>
+            <span>
+              Odsłuchaj zdanie wykorzystując Text-To-Speech.
+            </span>
+          </v-tooltip>
 
           <v-divider vertical></v-divider>
 
-          <v-btn flat icon @click="$emit('decreaseFontSize')">
-            <v-icon>remove</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <v-btn slot="activator" flat icon @click="$emit('decreaseFontSize')">
+              <v-icon>remove</v-icon>
+            </v-btn>
+            <span>Zmniejsz czcionkę.</span>
+          </v-tooltip>
           <span>Aa</span>
-          <v-btn flat icon @click="$emit('increaseFontSize')">
-            <v-icon>add</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <v-btn slot="activator" flat icon @click="$emit('increaseFontSize')">
+              <v-icon>add</v-icon>
+            </v-btn>
+            <span>Zwiększ czcionkę.</span>
+          </v-tooltip>
         </v-item-group>
       </v-flex>
 
       <v-flex sm1 class="text-xs-center">
-        <v-btn flat icon @click="$emit('nextSentence')">
-          <v-icon>arrow_forward</v-icon>
-        </v-btn>
+        <v-tooltip bottom>
+          <v-btn slot="activator" flat icon @click="$emit('nextSentence')">
+            <v-icon>arrow_forward</v-icon>
+          </v-btn>
+          <span>Przejdź do następnego zdania.</span>
+        </v-tooltip>
       </v-flex>
     </v-layout>
   </nav>
