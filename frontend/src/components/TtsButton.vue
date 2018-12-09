df5fc55e1fd2ef652879b5beb059b363f21ddeb5
Beastie <u@1ux.org>
2018-12-09 14:07:17 +0100

    Add TTS button component

---
+++ frontend/src/components/TtsButton.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<v-btn
+			color="info"
+			fab
+			large
+			dark
+			v-show="txt !== null && !locked"
+			id="all"
+			v-on:click="getTTS"
+			>
+			<v-icon>hearing</v-icon>
+		</v-btn>
+		<v-progress-circular
+			:size="50"
+			color="primary"
+			indeterminate
+			v-show="txt !== null && locked"
+			>
+		</v-progress-circular>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		locked: false,
+		src: "safdsffds"}),
+	props: ['id', 'txt'],
+	methods: {
+		fetchTTS: function(txt, assign) {
+			fetch("//localhost:10080/newaudio", {
+				method: "POST",
+				mode: "cors",
+				headers: {
+					"Content-Type": "plaintext",
+					"Access-Control-Allow-Origin": "*"
+				},
+				body: txt
+			}).then(
+				response => assign(response)
+			)/*.then(
+				success => {
+					//console.log(success);
+				}
+			).catch(
+				error => console.log(error)
+			);*/
+		},
+		playTTS: function(filename) {
+			const src = "//localhost:10080/audio/" + filename;
+			const audio = new Audio(src);
+			audio.play();
+		},
+		getTTS: function() {
+			this.locked = true;
+			let txt = "";
+			//if (myid == "all")
+			//	txt = this.currentDocument.text;
+			txt = this.txt;
+			if (this.src == null || !this.src)
+				this.fetchTTS(txt, function(d) {
+					this.src = d.body;
+					this.playTTS(this.src);
+					this.locked = false;
+				});
+			else {
+				this.playTTS(this.src);
+				this.locked = false;
+			}
+		}
+	}
+}
+</script>
